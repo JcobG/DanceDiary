4105aa693f5d8727984d89f0018ec7d659b88a37
JcobG <151858966+JcobG@users.noreply.github.com>
2025-01-29 00:17:00 +0100

    Create Note.php

---
+++ DanceDiary/backend/models/Note.php
@@ -0,0 +1,31 @@
+<?php
+class Note {
+    private $conn;
+    private $table = "notes";
+
+    public function __construct($db) {
+        $this->conn = $db;
+    }
+
+  public function addNote($userId, $content) {
+      $query = "INSERT INTO " . $this->table . " (user_id, content) 
+                VALUES (:user_id, :content)";
+      $stmt = $this->conn->prepare($query);
+  
+      $stmt->bindParam(':user_id', $userId);
+      $stmt->bindParam(':content', $content);
+  
+      return $stmt->execute();
+  }
+
+
+  public function getUserNotes($userId) {
+      $query = "SELECT * FROM " . $this->table . " WHERE user_id = :user_id";
+      $stmt = $this->conn->prepare($query);
+      $stmt->bindParam(':user_id', $userId);
+  
+      $stmt->execute();
+      return $stmt->fetchAll(PDO::FETCH_ASSOC);
+  }
+
+}
