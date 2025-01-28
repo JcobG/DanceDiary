b4c997078984ed3c6c85169466fa37439ff61d90
JcobG <151858966+JcobG@users.noreply.github.com>
2025-01-28 12:27:51 +0100

    Update schema.sql
    tworzenie wszystkich potrzebnych tabel

--- DanceDiary/database/schema.sql
+++ DanceDiary/database/schema.sql
@@ -1,16 +1,25 @@
+-- Tabela użytkowników
 CREATE TABLE users (
     user_id SERIAL PRIMARY KEY,
     first_name VARCHAR(50) NOT NULL,
     last_name VARCHAR(50) NOT NULL,
     email VARCHAR(100) UNIQUE NOT NULL,
     password_hash VARCHAR(255) NOT NULL,
-    role VARCHAR(20) CHECK (role IN ('tancerz', 'trener')) NOT NULL
+    role VARCHAR(20) CHECK (role IN ('tancerz', 'trener')) NOT NULL,
+    studio_id INT REFERENCES studios(studio_id) ON DELETE SET NULL
 );
+
+-- Tabela studiów tanecznych
 CREATE TABLE studios (
     studio_id SERIAL PRIMARY KEY,
     name VARCHAR(100) NOT NULL,
-    address TEXT NOT NULL
+    address TEXT NOT NULL,
+    description TEXT,
+    latitude DECIMAL(9, 6),
+    longitude DECIMAL(9, 6)
 );
+
+-- Tabela rezerwacji
 CREATE TABLE reservations (
     reservation_id SERIAL PRIMARY KEY,
     user_id INT REFERENCES users(user_id) ON DELETE CASCADE,
