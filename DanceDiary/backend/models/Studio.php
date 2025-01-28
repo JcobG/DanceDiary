10ca2309fe98c38c8e341ec58a1002fb42f4da89
JcobG <151858966+JcobG@users.noreply.github.com>
2025-01-29 00:13:56 +0100

    Create Studio.php

---
+++ DanceDiary/backend/models/Studio.php
@@ -0,0 +1,17 @@
+<?php
+class Studio {
+    private $conn;
+    private $table = "studios";
+
+    public function __construct($db) {
+        $this->conn = $db;
+    }
+
+public function getAllStudios() {
+    $query = "SELECT * FROM " . $this->table;
+    $stmt = $this->conn->prepare($query);
+
+    $stmt->execute();
+    return $stmt->fetchAll(PDO::FETCH_ASSOC);
+}
+}
