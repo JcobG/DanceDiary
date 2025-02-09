de3f1c6aa8a249c96297093d80e371e6e85d51d8
JcobG <151858966+JcobG@users.noreply.github.com>
2025-02-09 12:41:57 +0100

    Update app.js

--- DanceDiary/frontend/app.js
+++ DanceDiary/frontend/app.js
@@ -1,3 +1,9 @@
-document.addEventListener('DOMContentLoaded', () => {
-    console.log('DanceDiary loaded');
+document.addEventListener("DOMContentLoaded", function () {
+    console.log("DanceDiary Loaded");
+
+    // Jeśli użytkownik jest zalogowany, przekieruj go do dashboardu
+    if (localStorage.getItem("user")) {
+        document.querySelector("#login-link").style.display = "none";
+        document.querySelector("#dashboard-link").style.display = "block";
+    }
 });
